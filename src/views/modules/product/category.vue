cc1671faed0daab8f2906ad80a79736dc9c553cb
zhangcheng <zhengcheng@tongyuan.cc>
2023-12-13 17:19:34 +0800

    修改了 category.vue

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,43 +1,81 @@
 <template>
-    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree
+    :data="menus"
+    :props="defaultProps"
+    @node-click="handleNodeClick"
+    :expand-on-click-node="false"
+    :render-content="renderContent"
+    show-checkbox
+    node-key="catId"
+  >
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button
+          v-if="node.level <= 2"
+          type="text"
+          size="mini"
+          @click="() => append(data)"
+        >
+          Append
+        </el-button>
+        <el-button
+          v-if="node.childNodes.length == 0"
+          type="text"
+          size="mini"
+          @click="() => remove(node, data)"
+        >
+          Delete
+        </el-button>
+      </span>
+    </span>
+  </el-tree>
 </template>
 
 <script>
 export default {
-    name: 'category',
-    components: {},
-    directives: {},
-     data() {
-      return {
-        data: [],
-        defaultProps: {
-          children: 'children',
-          label: 'label'
-        }
-      };
-    },
-    mounted() {
-        
-    },
-    methods: {
-        handleNodeClick(data) {
-        console.log(data);
+  name: "category",
+  components: {},
+  directives: {},
+  data() {
+    return {
+      menus: [],
+      defaultProps: {
+        children: "children",
+        label: "name",
       },
-      getMenus(){
-        this.$http({
-          url: this.$http.adornUrl('/product/category/list/tree'),
-          method: 'get'
-        }).then(data=>{
-            console.log(data)
-        })
-      }
+    };
+  },
+  mounted() {
+    this.getMenus();
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
     },
-    created(){
-        this.getMenus();
-    }
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then(({ data }) => {
+        console.log(data);
+        this.menus = data.data;
+      });
+    },
+    append(data) {
+      console.log(data);
+    },
+
+    remove(node, data) {
+      console.log(node, data);
+    },
+
+    created() {
+      this.getMenus();
+    },
+  },
 };
 </script>
 
 <style scoped>
-
 </style>
